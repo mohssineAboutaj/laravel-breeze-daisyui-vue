a8aacf05ae6dd061fee41d5dde097d0e7fd48b4e
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2023-08-27 16:19:18 +0100

    feat(UI): update layouts UI and applogo

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,20 +1,58 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from "@inertiajs/vue3";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { computed } from "vue";
+
+// props
+defineProps({
+  title: { type: String, default: "Laravel" },
+  container: { type: Boolean, default: true },
+  fullFlex: { type: Boolean, default: false },
+});
+
+const page = usePage();
+
+// computed
+const canLogin = computed(() => page.props.can.login);
+const canRegister = computed(() => page.props.can.register);
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
-        </div>
-
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
-        >
-            <slot />
-        </div>
+  <nav class="navbar bg-base-100 fixed">
+    <div class="flex-1">
+      <Link
+        :href="route('welcome')"
+        class="flex items-center gap-x-2 normal-case text-xl"
+      >
+        <ApplicationLogo />
+        <span>{{ title }}</span>
+      </Link>
     </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li v-if="$page.props.auth.user">
+          <Link :href="route('dashboard')">Dashboard</Link>
+        </li>
+
+        <template v-else>
+          <li v-if="canLogin">
+            <Link :href="route('login')">Login</Link>
+          </li>
+
+          <li v-if="canRegister">
+            <Link v-if="canRegister" :href="route('register')">Register</Link>
+          </li>
+        </template>
+      </ul>
+    </div>
+  </nav>
+
+  <main
+    :class="{
+      container,
+      'flex items-center min-h-screen': fullFlex,
+    }"
+  >
+    <slot />
+  </main>
 </template>
