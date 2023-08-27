79154667f5323b01be763dbe946b23365fdddb82
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2023-08-27 16:17:12 +0100

    feat: add names to routes

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,42 +3,50 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
-    /**
-     * The root template that is loaded on the first page visit.
-     *
-     * @var string
-     */
-    protected $rootView = 'app';
+  /**
+   * The root template that is loaded on the first page visit.
+   *
+   * @var string
+   */
+  protected $rootView = 'app';
 
-    /**
-     * Determine the current asset version.
-     */
-    public function version(Request $request): string|null
-    {
-        return parent::version($request);
-    }
+  /**
+   * Determine the current asset version.
+   */
+  public function version(Request $request): string|null
+  {
+    return parent::version($request);
+  }
 
-    /**
-     * Define the props that are shared by default.
-     *
-     * @return array<string, mixed>
-     */
-    public function share(Request $request): array
-    {
-        return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => $request->user(),
-            ],
-            'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
-                    'location' => $request->url(),
-                ]);
-            },
+  /**
+   * Define the props that are shared by default.
+   *
+   * @return array<string, mixed>
+   */
+  public function share(Request $request): array
+  {
+    return array_merge(parent::share($request), [
+      'auth' => [
+        'user' => $request->user(),
+      ],
+      'ziggy' => function () use ($request) {
+        return array_merge((new Ziggy)->toArray(), [
+          'location' => $request->url(),
         ]);
-    }
+      },
+      'can' => [
+        'login' => Route::has('login'),
+        'register' => Route::has('register'),
+      ],
+      'env' => [
+        'title' => env('APP_NAME'),
+      ]
+    ]);
+  }
 }
